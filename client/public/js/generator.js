e6c1fce1cefdfb45c5e838038ce4310ffcdb1315
Frank Matranga <thefrankmatranga@gmail.com>
2018-06-11 22:08:41 -0400

    VueJS and Form start

--- client/public/js/generator.js
+++ client/public/js/generator.js
@@ -2,7 +2,20 @@ const generator_app = new Vue({
   el: '#generator',
   name: 'book-generator',
   data: {
-
+    new_teacher: '',
+    new_student: '',
+    book: {
+      title: 'Test',
+      introduction: '',
+      coverImage: null
+    },
+    location: {
+      name: '',
+      description: '',
+      image: null
+    },
+    teachers: [],
+    students: []
   },
   methods: {
 
