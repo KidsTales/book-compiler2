5156928f4f468f2a68cbc7026965d14a597af6a6
Frank Matranga <thefrankmatranga@gmail.com>
2018-06-11 23:11:22 -0400

    Hold files

--- client/public/js/generator.js
+++ client/public/js/generator.js
@@ -18,6 +18,12 @@ const generator_app = new Vue({
     students: []
   },
   methods: {
+    changeCoverImage: function (event) {
+      this.book.coverImage = event.target.files[0];
+    },
+    changeLocationImage: function (event) {
+      this.location.image = event.target.files[0];
+    },
     addTeacher: function (event) {
       if (this.new_teacher.length === 0) return;
       this.teachers.push(this.new_teacher);
@@ -43,6 +49,9 @@ const generator_app = new Vue({
     removeStudent: function (index) {
       if (!confirm(`Are you sure you want to remove student ${this.students[index].name}?`)) return;
       this.students.splice(index, 1);
+    },
+    changeStudentImage: function (event, index) {
+      this.students[index].image = event.target.files[0];
     }
   },
   computed: {
